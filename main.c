5ec007b8526ba4843c79dacc86e8718f63c48f9c
Morrantho <morrantho@gmail.com>
2020-08-23 13:20:06 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
+#include <stdio.h>
+#include <windows.h>
 /******************************************************************************
 Util
 ******************************************************************************/
-#define true 1
-#define false 0
+#define dbg 1
 typedef unsigned int ui;
 typedef unsigned char bool,byte;
 /******************************************************************************
 Fwd Decl / Limits
 ******************************************************************************/
 struct world;
-typedef void (*sysfunc)(bool *);
+typedef void (*systick)(ui ent);
+#define TICKMS 32
 #define MAXENTS 1024
 #define MAXSYS 16
 #define MAXCMP 32
@@ -31,60 +33,77 @@ Masks
 /******************************************************************************
 Structs
 ******************************************************************************/
-typedef struct system
+typedef struct system_t
 {
 	ui mask;
-	bool entities[MAXENTS];
-	sysfunc func;
-} system;
-
-typedef struct world // world struct manages literally everything. if it goes out of scope, rip.
+	systick tick;
+} system_t;
+typedef struct world 
 {
 	ui entities[MAXENTS];
-	system systems[MAXSYS];
-	// start components
-	double size[MAXENTS][2]; // components are literally just arrays here, not structs. optionally make structs and pretty members, but this is way cleaner.
+	system_t systems[MAXSYS];
+	double size[MAXENTS][2]; 
 	double pos[MAXENTS][2];
 	byte col[MAXENTS][3];
 	double vel[MAXENTS][2];
 	ui hp[MAXENTS];
-	// end components
 } world;
 /******************************************************************************
 Lib Methods
 ******************************************************************************/
-void sys_reg(world *w,ui mask,sysfunc func)
+void sys_reg(world *w,ui mask,systick tick)
 {
-	ui id = 0;
-	for(;w->systems[id].mask != 0 && id < MAXSYS;id++)
-	w->systems[id].mask = mask;
-	w->systems[id].func = func;
-}
-void sys_match(world *w,ui ent)
-{
-	ui entmask = w->entities[ent];
-	for(ui i=0;i < MAXSYS;i++)
-	{
-		ui sysmask = w->systems[i].mask;
-		w->systems[i].entities[ent] = entmask == sysmask;
-	}
+	ui i = 0;
+	for(;i<MAXSYS;i++) if(w->systems[i].mask == 0) break;
+	w->systems[i].mask = mask;
+	w->systems[i].tick = tick;
+	#ifdef dbg
+	printf("sys_reg: %d\n",i);
+	#endif
 }
 ui ent_reg(world *w)
 {
-	ui id = 0;
-	for(;w->entities[id] != 0 && id < MAXENTS;id++)
-	return id;
+	ui i = 0;
+	for(;i<MAXENTS;i++)
+	{
+		if(w->entities[i] == 0)
+		{
+			#ifdef dbg
+			printf("ent_reg: %d\n",i);
+			#endif
+			return i;
+		}
+	}
+	return i;
 }
 bool cmp_has(world *w,ui ent,ui cmp)
 {
 	return (w->entities[ent] & cmp) == cmp;
 }
-void cmp_mgr(world *w,ui ent,ui cmp) // toggles adding / removing one or multiple components. can be a mask for all it cares.
+void cmp_mgr(world *w,ui ent,ui cmp)
 {
 	ui or  = w->entities[ent]|cmp;
 	ui xor = w->entities[ent]^cmp;
-	w->entities[ent] = cmp_has(w,ent,cmp) ? xor:or;
-	sys_match(w,ent);
+	ui res = cmp_has(w,ent,cmp) ? xor:or;
+	#ifdef dbg
+	printf("cmp_mgr: ent:%d, cmp:%d added:%d\n",ent,cmp,res);
+	#endif
+	w->entities[ent] = res;
+}
+void world_tick(world *w)
+{
+	for(ui i=0;i<MAXENTS;i++)
+	{
+		if(w->entities[i] == 0) continue;
+		ui entmask = w->entities[i];
+		for(ui j=0;j<MAXSYS;j++)
+		{
+			ui sysmask = w->systems[j].mask;
+			if(sysmask == 0) continue;
+			if((entmask&sysmask) != sysmask) continue;
+			w->systems[j].tick(i);
+		}
+	}
 }
 /******************************************************************************
 Implementation / Usage
@@ -94,91 +113,45 @@ Global
 **************************************/
 world w;
 /**************************************
-Base Entity Methods
-**************************************/
-void ent_setsize(world *_w,ui ent,double w,double h)
-{
-	_w->size[ent][0] = w;
-	_w->size[ent][1] = h;
-}
-double *ent_getsize(world *w,ui ent)
-{
-	return w->size[ent];
-}
-void ent_setpos(world *w,ui ent,double x,double y)
-{
-	w->pos[ent][0] = x;
-	w->pos[ent][1] = y;
-}
-double *ent_getpos(world *w,ui ent)
-{
-	return w->pos[ent];
-}
-void ent_setcol(world *w,ui ent,byte r,byte g,byte b)
-{
-	w->col[ent][0] = r;
-	w->col[ent][1] = g;
-	w->col[ent][2] = b;
-}
-byte *ent_getcol(world *w,ui ent)
-{
-	return w->col[ent];
-}
-void ent_setvel(world *w,ui ent,double vx,double vy)
-{
-	w->vel[ent][0] = vx;
-	w->vel[ent][1] = vy;
-}
-double *ent_getvel(world *w,ui ent)
-{
-	return w->vel[ent];
-}
-/**************************************
-Niche Methods
-**************************************/
-void ent_sethp(world *w,ui ent,ui hp)
-{
-	w->hp[ent] = hp;
-}
-ui ent_gethp(world *w,ui ent)
-{
-	return w->hp[ent];
-}
-/**************************************
-Objects / Init Methods? w/e you want, really. just wraps calls.
+Player
 **************************************/
 void player_init(world *w,ui ent)
 {
-	ent_setsize(w,ent,32.0,32.0);
-	ent_setpos(w,ent,256.0,256.0);
-	ent_setvel(w,ent,0.0,0.0);
-	ent_setcol(w,ent,255,255,255); // pasty ass
-	ent_sethp(w,ent,100);
-	cmp_mgr(w,ent,PLAYER_MASK); // add all of these, O(1) who cares.
+	w->size[ent][0] = 32;
+	w->size[ent][1] = 32;
+	w->col[ent][0] = 255;
+	w->col[ent][1] = 255;
+	w->col[ent][2] = 255;
+	w->hp[ent] = 100;
+	w->pos[ent][0] = 0;
+	w->pos[ent][1] = 0;
+	w->vel[ent][0] = 0;
+	w->vel[ent][1] = 0;
+	cmp_mgr(w,ent,PLAYER_MASK);
 }
 /**************************************
 Systems
 **************************************/
-void sys_collide()
+void game_loop() // just an example, not really practical
 {
-	for(ui i=0;i<MAXENTS;i++)
+	while(1)
 	{
-
+		world_tick(&w);
+		Sleep(TICKMS);
 	}
 }
-void sys_render()
-{
-	for(ui i=0;i<MAXENTS;i++)
-	{
 
-	}
+void sys_collide(ui ent)
+{
+	printf("sys_collide: %d\n",ent);
 }
-void sys_damage()
+void sys_render(ui ent)
 {
-	for(ui i=0;i<MAXENTS;i++)
-	{
-
-	}	
+	printf("sys_render: %d\n",ent);
+}
+void sys_damage(ui ent)
+{
+	printf("sys_damage: %d\n",ent);	
 }
 int main(int argc, char **argv)
 {
@@ -188,6 +161,7 @@ int main(int argc, char **argv)
 	sys_reg(&w,DAMAGE_MASK,sys_damage);
 	// create a player ent
 	player_init(&w,ent_reg(&w));
-	// literally just make a game loop and loop systems and call their callbacks.
+	// start game loop
+	game_loop();
 	return 0;
 }
